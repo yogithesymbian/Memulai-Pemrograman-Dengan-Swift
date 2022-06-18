a91718c266701942214e9e76c88f2db2c9650436
yogithesymbian <yogirenbox33@gmail.com>
2022-06-18 10:36:27 +0800

    ARC ( Automatic Reference Counting )

--- MyPlayground.playground/Pages/memory arc.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/memory arc.xcplaygroundpage/Contents.swift
@@ -78,8 +78,72 @@ class PhoneNumber {
    deinit { print("Phone number +\(number) is being deinitialized") }
 }
 
-var memberDicoding: PersonA? // nil / opsional
-memberDicoding = PersonA(name: "Yogi Arif Widodo")
-memberDicoding!.number = PhoneNumber(number: 62810_8108_1081, owner: memberDicoding!)
+var memberDicodingA: PersonA? // nil / opsional
+memberDicodingA = PersonA(name: "Yogi Arif Widodo")
+memberDicodingA!.number = PhoneNumber(number: 62810_8108_1081, owner: memberDicodingA!)
 
-memberDicoding = nil
+memberDicodingA = nil
+
+// MARK: memory safety
+var stepSize = 1
+func increment(_ number: inout Int) {
+   number += stepSize
+}
+
+// Buat salinan secara eksplisit.
+var copyOfStepSize = stepSize
+increment(&copyOfStepSize)
+
+// Perbarui variabel yang asli.
+stepSize = copyOfStepSize
+// stepSize sekarang bernilai 2
+
+
+func balance(_ x: inout Int, _ y: inout Int) {
+   let sum = x + y
+   x = sum / 2
+   y = sum - x
+}
+
+var playerOneScore = 42
+var playerTwoScore = 30
+
+balance(&playerOneScore, &playerTwoScore)  // OK
+//balance(&playerOneScore, &playerOneScore)
+// Error: conflicting accesses to playerOneScore
+
+// MARK: Conflicting Access to self in Methods
+struct Player {
+   var name: String
+   var health: Int
+   var energy: Int
+   static let maxHealth = 10
+   mutating func restoreHealth() {
+       health = Player.maxHealth
+   }
+}
+extension Player {
+   mutating func shareHealth(with teammate: inout Player) {
+       balance(&teammate.health, &health)
+   }
+}
+var gilang = Player(name: "Gilang", health: 10, energy: 10)
+var ramadhan = Player(name: "Ramadhan", health: 5, energy: 10)
+gilang.shareHealth(with: &ramadhan)  // O
+//gilang.shareHealth(with: &gilang)
+// Error: conflicting accesses to gilang
+
+// MARK: Conflicting Access to Properties
+//var playerInformation = (health: 10, energy: 20)
+//balance(&playerInformation.health, &playerInformation.energy)
+// Error: conflicting access to properties of playerInformation
+
+//var yourName = Player(name: "Nama kamu", health: 10, energy: 10)
+//balance(&yourName.health, &yourName.energy)  // Error
+
+//func someFunction() {
+//   var gilang = Player(name: "Gilang", health: 10, energy: 10) // local variable for overlapse is safety
+//   balance(&gilang.health, &gilang.energy)  // OK
+//}
+//
+
